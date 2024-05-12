c047f7cac2e385df9ff358dc593dc8746b01c917
suzan-sheikh <mdsuzanskh@gmail.com>
2024-05-12 18:21:08 +0600

    delete a data from mongoDB oparation complited

--- index.js
+++ index.js
@@ -46,6 +46,15 @@ async function run() {
       res.send(result)
     })
 
+    // get data filter by email 
+    app.get('/jobs/:email', async(req, res) => {
+      const email = req.params.email
+      const query = {'buyer.email': email }
+      const result = await jobsCollection.find(query).toArray()
+      res.send(result)
+    })
+
+
     // save job to mongoDB
     app.post('/job', async(req, res) => {
       const jobData = req.body;
@@ -59,6 +68,16 @@ async function run() {
       const result = await appliedJobCollection.insertOne(postData)
       res.send(result)
     })
+
+    // delete a job data
+    app.delete('/job/:id', async(req, res) => {
+      const id = req.params.id
+      const query = {_id: new ObjectId(id)}
+      const result = await jobsCollection.deleteOne(query)
+      res.send(result)
+    })
+
+
 
     
 
